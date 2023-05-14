5b045543e5c23e8c0fdf2d813b6f6d7cb9e80998
汪小乐 <xiaole.wang@djhealthunion.com.cn>
2023-05-14 23:29:04 +0800

    gateway面向服务的路由

---
+++ 05-gateway-eureka/gateway-server/src/main/java/com/example/gatewayserver/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: GatewayApplication
+ * @Description: GatewayApplication
+ * @Author: wang xiao le
+ * @Date: 2023/05/14 16:20
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
