788a01fc923684bd57f6d548f0e72bad464041c2
汪小乐 <xiaole.wang@djhealthunion.com.cn>
2023-05-15 22:06:29 +0800

    gateway 限流

---
+++ 05-gateway-eureka/gateway-server/src/main/java/com/example/gatewayserver/config/IpKeyResolver.java
@@ -0,0 +1,13 @@
+package com.example.gatewayserver.config;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class IpKeyResolver implements KeyResolver {
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    }
+}
